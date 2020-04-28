1c2a882dfbdd886923c84dabb72f367d34cbf2b3
Tien <viettienha98@gmail.com>
2020-04-28 17:21:39 +0700

    Add Upload component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Title from "./Title.js";
+import Upload from "./Upload.js";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Upload />
     </div>
   );
 }
