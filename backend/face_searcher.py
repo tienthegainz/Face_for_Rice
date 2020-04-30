Fix wrong shape of result in graph

backend/face_searcher.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class FaceSearcher(object):
-    def __init__(self, dim=512, space='l2', threshold=0.5, k=1):
+    def __init__(self, dim=512, space='l2', threshold=0.5):
         """
             Args:
                 dim: face embedding feature length
@@ -13,7 +13,7 @@ class FaceSearcher(object):
         self.p = hnswlib.Index(space=space, dim=dim)
         self.p.init_index(max_elements=1000, ef_construction=200, M=48)
         self.p.set_ef(20)
-        self.k = k
+        self.k = 1
         self.threshold = threshold
 
     def add_faces(self, data, index):
@@ -31,9 +31,11 @@ class FaceSearcher(object):
     def query_faces(self, data):
         try:
             index, distance = self.p.knn_query(data, k=self.k)
-            # print(index, ' --> ', distance)
             # Filter result
-            return index[distance < self.threshold]
+            index = np.squeeze(index)
+            distance = np.squeeze(distance)
+            print('Index: ', index, '\nDistance: ', distance)
+            return index if distance < self.threshold else None
         except Exception as err:
             # TODO: Logging here
             print(err)

backend/main.py
@@ -12,6 +12,7 @@ from PIL import Image
 from torchvision.transforms import ToTensor, ToPILImage
 import random
 import string
+import base64
 
 # initialize our Flask application
 app = Flask(__name__)
@@ -78,7 +79,9 @@ def analyze_face():
         face = fd.extract_face(image, false_path)
         s_idx = fs.query_faces(face)
 
-        if not s_idx:
+        print('\nQuery result: {}\n')
+
+        if s_idx is None:
             data['permission'] = True
             data['success'] = True
             face = np.expand_dims(face, axis=0)
@@ -94,9 +97,6 @@ def analyze_face():
         else:
             data['permission'] = False
             data['success'] = True
-            # TODO: Send back image data
-            data['duplicate'] = img_path_map[s_idx[0]]
-            print(s_idx)
             print('Saved ', false_path)
 
     return jsonify(data)